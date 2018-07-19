2e9027bd6a9f4d6ce93b1f62902b745a4a545875
habibliu <740033486@qq.com>
2018-07-19 19:36:11 +0800

    生产环境

--- src/common/js/AxiosPlugin.js
+++ src/common/js/AxiosPlugin.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 import { catchError } from '@/common/js/util.js'
 
 const Axios = axios.create({
-  baseUrl:process.env.API_URL,
+  baseUrl:'http://39.159.214.95:8100',
   timeout: 60000
 })
 
